bc34682257a5cd4d3090af6263017b8bd543f9e3
himanshu <hp746123@gmail.com>
2017-02-26 01:14:24 +0530

    full of errors

--- app/src/main/java/com/example/abans_000/receiver/EventActivity.java
+++ app/src/main/java/com/example/abans_000/receiver/EventActivity.java
@@ -3,21 +3,34 @@ package com.example.abans_000.receiver;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.abans_000.receiver.services.ScheduleClient;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class EventActivity extends AppCompatActivity {
+
+    private ScheduleClient mScheduleClient;
+
+    private long startTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
 
+        mScheduleClient = new ScheduleClient(this);
+        mScheduleClient.doBindService();
+
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
-        long startTime = intent.getLongExtra("stime", System.currentTimeMillis());
+        startTime = intent.getLongExtra("stime", System.currentTimeMillis());
         long finishTime = intent.getLongExtra("ftime", System.currentTimeMillis());
         String description = intent.getStringExtra("description");
 
@@ -34,5 +47,39 @@ public class EventActivity extends AppCompatActivity {
         finishTextView.setText(formatter.format(new Date(finishTime)));
         descTextView.setText(description);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.event_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_reminder:
+                // Create a new calendar set to the date chosen
+                // we set the time to midnight (i.e. the first minute of that day)
+                Calendar d = Calendar.getInstance();
+                d.setTimeInMillis(startTime);
+
+                // Ask our service to set an alarm for that date, this activity talks to the client that talks to the service
+                mScheduleClient.setAlarmForNotification(d);
+                // Notify the user what they just did
+                Toast.makeText(getApplicationContext(), "Reminder set successfull", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        // When our activity is stopped ensure we also stop the connection to the service
+        // this stops us leaking our activity into the system *bad*
+        if(mScheduleClient != null)
+            mScheduleClient.doUnbindService();
+        super.onStop();
     }
 }
